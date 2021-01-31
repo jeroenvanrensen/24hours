5358d2378af8315093d0e914c6ba0edd88c84367
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-01-31 20:23:34 +0100

    Delete floating button

--- resources/views/components/floating-button.blade.php
+++
@@ -1,5 +0,0 @@
-<button {{ $attributes->merge([
-    'class' => 'fixed bottom-48 right-12 p-3 bg-gray-400 hover:bg-gray-500 rounded-full transition focus:outline-none focus:bg-gray-600 text-white'
-]) }}>
-    {{ $slot }}
-</button>

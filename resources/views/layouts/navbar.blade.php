de469757615a7e9369e651a1a7d055a89da9aea7
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-02-04 19:39:03 +0100

    Make the navbar responsive

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="py-4 px-6 md:px-12 flex items-center justify-between">
-    <a href="{{ auth()->check() ? route('boards.index') : route('home') }}" class="font-serif focus:underline focus:outline-none tracking-wide text-xl w-0">24Hours</a>
+    <a href="{{ auth()->check() ? route('boards.index') : route('home') }}" class="font-serif focus:underline focus:outline-none tracking-wide text-xl md:w-0">24Hours</a>
 
     <ul class="flex">
         @auth
             <x-layouts.nav-link url="{{ route('boards.index') }}">
                 <x-slot name="icon"><svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg></x-slot>
-                Home
+                <span class="hidden md:inline">Home</span>
             </x-layouts.nav-link>
             
             <x-layouts.nav-link url="{{ route('search') }}">
                 <x-slot name="icon"><svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg></x-slot>
-                Search
+                <span class="hidden md:inline">Search</span>
             </x-layouts.nav-link>
         @else
             <x-layouts.nav-link url="{{ route('login') }}">
@@ -26,7 +26,7 @@
     </ul>
 
     @auth
-        <a href="{{ route('profile.edit') }}" class="p-2 rounded-full bg-gray-100 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 text-gray-600">
+        <a href="{{ route('profile.edit') }}" class="p-2 rounded-full bg-gray-100 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 text-gray-600 md:w-0">
             <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
             </svg>

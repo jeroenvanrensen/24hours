3d2771a0dce28044ed2d4223394c01a28cf8d3d9
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-02-05 09:28:39 +0100

    Add texts to the landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
     <!-- Hero -->
     <div class="mt-8 md:mt-32 max-w-6xl mx-auto px-6">
         <div class="text-center">
-            <h1 class="text-3xl md:text-5xl font-bold">Lorem ipsum dolor sit amet.</h1>
-            <p class="mt-4 mb-8 md:mt-8 md:mb-10 text-gray-700 dark:text-gray-400 text-lg max-w-4xl mx-auto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, esse. Odio voluptas, labore repellat accusantium totam perspiciatis quis voluptate, praesentium ducimus et quisquam. Alias molestiae harum, nisi quam sit laborum.</p>
+            <h1 class="text-3xl md:text-5xl font-bold">Bring your projects to life.</h1>
+            <p class="mt-4 mb-8 md:mt-8 md:mb-10 text-gray-700 dark:text-gray-400 text-lg max-w-xl mx-auto">24Hours is a productivity tool designed to keep your projects moving forward and your head clear.</p>
             <x-button link href="{{ route('register') }}" class="py-3">Sign up for free</x-button>
         </div>
         
@@ -18,51 +18,37 @@
     <!-- Features -->
     <div class="py-12 md:py-24 max-w-5xl mx-auto px-6">
         <div class="mb-8 md:mb-16 md:text-center">
-            <h2 class="text-blue-800 dark:text-blue-400 font-semibold uppercase tracking-wide">Lorem, ipsum</h2>
-            <p class="mt-2 mb-4 md:mt-4 text-3xl leading-8 font-bold tracking-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet consectetur.</p>
+            <h2 class="text-blue-800 dark:text-blue-400 font-semibold uppercase tracking-wide">Features</h2>
+            <p class="mt-2 mb-4 md:mt-4 text-3xl leading-8 font-bold tracking-tight text-gray-900 dark:text-white">Space to organize all your ideas.</p>
         </div>
 
         <div class="md:grid grid-cols-2 gap-8 lg:gap-6">
             <!-- Grid item -->
             <x-home.feature>
-                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg></x-slot>
-                <x-slot name="title">Lorem, ipsum dolor.</x-slot>
-                <x-slot name="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident id minus, officiis reiciendis vitae eligendi.</x-slot>
+                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" /></svg></x-slot>
+                <x-slot name="title">Organize everything</x-slot>
+                <x-slot name="description">You can create unlimited boards to organize every project.</x-slot>
             </x-home.feature>
 
             <!-- Grid item -->
             <x-home.feature>
-                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg></x-slot>
-                <x-slot name="title">Lorem ipsum dolor sit amet.</x-slot>
-                <x-slot name="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis amet blanditiis, itaque accusantium dolore facere odit cumque, inventore maiores repudiandae quos. Iste praesentium aperiam fugiat.</x-slot>
+                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" /></svg></x-slot>
+                <x-slot name="title">Save from the web</x-slot>
+                <x-slot name="description">Found an article you want to read later? Save it and find it easily back.</x-slot>
             </x-home.feature>
 
             <!-- Grid item -->
             <x-home.feature>
-                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg></x-slot>
-                <x-slot name="title">Lorem ipsum dolor sit.</x-slot>
-                <x-slot name="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus cumque fugit minima explicabo eius necessitatibus autem nemo tempora quis beatae?</x-slot>
+                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg></x-slot>
+                <x-slot name="title">Clean your head</x-slot>
+                <x-slot name="description">Create notes to clear your head and write everything down in a nice editor.</x-slot>
             </x-home.feature>
 
             <!-- Grid item -->
             <x-home.feature>
-                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg></x-slot>
-                <x-slot name="title">Lorem, ipsum dolor.</x-slot>
-                <x-slot name="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident id minus, officiis reiciendis vitae eligendi.</x-slot>
-            </x-home.feature>
-
-            <!-- Grid item -->
-            <x-home.feature>
-                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg></x-slot>
-                <x-slot name="title">Lorem ipsum dolor sit amet.</x-slot>
-                <x-slot name="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis amet blanditiis, itaque accusantium dolore facere odit cumque, inventore maiores repudiandae quos. Iste praesentium aperiam fugiat.</x-slot>
-            </x-home.feature>
-
-            <!-- Grid item -->
-            <x-home.feature>
-                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg></x-slot>
-                <x-slot name="title">Lorem ipsum dolor sit.</x-slot>
-                <x-slot name="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus cumque fugit minima explicabo eius necessitatibus autem nemo tempora quis beatae?</x-slot>
+                <x-slot name="icon"><svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg></x-slot>
+                <x-slot name="title">Find anything</x-slot>
+                <x-slot name="description">Lost anything? Find it back with the powerful search feature.</x-slot>
             </x-home.feature>
         </div>
     </div>

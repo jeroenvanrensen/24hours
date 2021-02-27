5aa490d1567d8704566a3817923e075d02d6f9b3
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-02-27 11:46:57 +0100

    Update dark mode

--- resources/views/components/search/item.blade.php
+++ resources/views/components/search/item.blade.php
@@ -1,8 +1,8 @@
 @props(['newTab' => false, 'loop' => null])
 
 <a href="{{ $url }}" class="flex items-center py-4 {{ $loop->first ? '' : 'border-t border-gray-200 dark:border-gray-700' }} focus:outline-none focus:underline" {{ $newTab ? 'target="_blank"' : '' }}>
-    <div class="mr-3 p-2 bg-{{ $color }}-100 dark:bg-{{ $color }}-700 rounded-lg">
-        <div class="text-{{ $color }}-400">
+    <div class="mr-3 p-2 bg-{{ $color }}-100 dark:bg-{{ $color }}-600 rounded-lg">
+        <div class="text-{{ $color }}-400 dark:text-{{ $color }}-300">
             {{ $icon }}
         </div>
     </div>

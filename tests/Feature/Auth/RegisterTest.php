4d45e59e6969419f2291234bf1c3e55d9cdb447e
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-03-02 12:08:12 +0100

    An initial board and note is created when someone registeres

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Auth;
 
 use App\Http\Livewire\Auth\Register;
+use App\Models\Board;
+use App\Models\Note;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Notifications\VerifyEmail;
@@ -189,5 +191,76 @@ class RegisterTest extends TestCase
             ->set('password_confirmation', 'other-password') // wrong confirmation
             ->call('register')
             ->assertHasErrors('password');
+    }
+
+    /** @test */
+    public function after_registering_a_user_has_a_board()
+    {
+        $this->withoutExceptionHandling();
+        
+        $this->assertCount(0, Board::all());
+        
+        Livewire::test(Register::class)
+            ->set('name', 'John Doe')
+            ->set('email', 'john@example.org')
+            ->set('password', 'password')
+            ->set('password_confirmation', 'password')
+            ->call('register')
+            ->assertRedirect(route('invitations.check'));
+
+        $this->assertCount(1, Board::all());
+
+        $this->assertDatabaseHas('boards', [
+            'user_id' => User::first()->id,
+            'name' => 'Welcome'
+        ]);
+    }
+
+    /** @test */
+    public function after_registering_a_user_has_a_note()
+    {
+        $this->withoutExceptionHandling();
+        
+        $this->assertCount(0, Note::all());
+        
+        Livewire::test(Register::class)
+            ->set('name', 'John Doe')
+            ->set('email', 'john@example.org')
+            ->set('password', 'password')
+            ->set('password_confirmation', 'password')
+            ->call('register')
+            ->assertRedirect(route('invitations.check'));
+
+        $this->assertCount(1, Note::all());
+
+        $this->assertDatabaseHas('notes', [
+            'board_id' => Board::first()->id,
+            'title' => 'About 24Hours',
+            'body' => '<h1>About 24Hours</h1><p>Hello John!</p><p><br></p><p>On this note, I would like to tell you a bit more about 24Hours. How it works and when you should use it.</p><p><br></p><p><strong>I recommend keeping this note as it explains the core features of 24Hours.</strong></p><p><br></p><h2>Boards, notes, and links</h2><p>Think of a <strong>board </strong>as a space to organize a project. For example, if you\'re creating a new website there is a lot to be done, and you can create a board to save everything.</p><h3><br></h3><p><strong>Notes </strong>are... notes! You can add as many notes as you would like. You can use them to write whole essays or to quickly clean your head. <strong>Notes are auto-saved</strong>, so all your edits will be saved immediately.</p><h3><br></h3><p>If you found something on the internet that you want to save for later, you can save them as a <strong>link</strong>. 24Hours will automatically get the title and an image so you can quickly and easily see which link is what page.</p><p><br></p><h2>Search</h2><p>Using 24Hours powerful search feature you can find everything that you\'ve lost! Head over to the search page and go searching!</p><p><br></p><h2>Collaboration</h2><p>Working together is very easy using 24Hours\' collaboration feature. You can invite anyone, also people who don\'t have an account yet.</p><p><br></p><p>In a board there are three roles with their own permissions:</p><p><br></p><p><strong>Owner</strong>: this is the creator of the board. Only the owner can edit or delete the board and can invite (and remove) members and viewers.</p><p><br></p><p><strong>Member</strong>: a member can create, edit and delete notes and add and remove links. The member can <strong>not</strong> edit the board itself or invite other members and viewers.</p><p><br></p><p><strong>Viewer</strong>: a viewer can only view notes and links. They cannot edit anything.</p><p><br></p><h2>Support</h2><p>If you have any questions, feature requests, or anything else, please contact me at <a href="https://www.jeroenvanrensen.nl/contact" target="_blank">https://www.jeroenvanrensen.nl/contact</a>. </p><p><br></p><p>Thanks for using 24Hours,</p><p>Jeroen van Rensen</p>'
+        ]);
+    }
+
+    /** @test */
+    public function the_name_in_the_notes_salutation_is_correct()
+    {
+        $this->withoutExceptionHandling();
+        
+        $this->assertCount(0, Note::all());
+        
+        Livewire::test(Register::class)
+            ->set('name', 'Jane Lastname')
+            ->set('email', 'jane@example.org')
+            ->set('password', 'password')
+            ->set('password_confirmation', 'password')
+            ->call('register')
+            ->assertRedirect(route('invitations.check'));
+
+        $this->assertCount(1, Note::all());
+
+        $this->assertDatabaseHas('notes', [
+            'board_id' => Board::first()->id,
+            'title' => 'About 24Hours',
+            'body' => '<h1>About 24Hours</h1><p>Hello Jane!</p><p><br></p><p>On this note, I would like to tell you a bit more about 24Hours. How it works and when you should use it.</p><p><br></p><p><strong>I recommend keeping this note as it explains the core features of 24Hours.</strong></p><p><br></p><h2>Boards, notes, and links</h2><p>Think of a <strong>board </strong>as a space to organize a project. For example, if you\'re creating a new website there is a lot to be done, and you can create a board to save everything.</p><h3><br></h3><p><strong>Notes </strong>are... notes! You can add as many notes as you would like. You can use them to write whole essays or to quickly clean your head. <strong>Notes are auto-saved</strong>, so all your edits will be saved immediately.</p><h3><br></h3><p>If you found something on the internet that you want to save for later, you can save them as a <strong>link</strong>. 24Hours will automatically get the title and an image so you can quickly and easily see which link is what page.</p><p><br></p><h2>Search</h2><p>Using 24Hours powerful search feature you can find everything that you\'ve lost! Head over to the search page and go searching!</p><p><br></p><h2>Collaboration</h2><p>Working together is very easy using 24Hours\' collaboration feature. You can invite anyone, also people who don\'t have an account yet.</p><p><br></p><p>In a board there are three roles with their own permissions:</p><p><br></p><p><strong>Owner</strong>: this is the creator of the board. Only the owner can edit or delete the board and can invite (and remove) members and viewers.</p><p><br></p><p><strong>Member</strong>: a member can create, edit and delete notes and add and remove links. The member can <strong>not</strong> edit the board itself or invite other members and viewers.</p><p><br></p><p><strong>Viewer</strong>: a viewer can only view notes and links. They cannot edit anything.</p><p><br></p><h2>Support</h2><p>If you have any questions, feature requests, or anything else, please contact me at <a href="https://www.jeroenvanrensen.nl/contact" target="_blank">https://www.jeroenvanrensen.nl/contact</a>. </p><p><br></p><p>Thanks for using 24Hours,</p><p>Jeroen van Rensen</p>'
+        ]);
     }
 }

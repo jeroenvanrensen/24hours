41bf129371f8d5f84e36048dd741f6ac203ae486
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-04-03 14:57:56 +0200

    Remove doc blocks

--- app/Http/Livewire/Profile/Password.php
+++ app/Http/Livewire/Profile/Password.php
@@ -7,51 +7,21 @@ use Livewire\Component;
 
 class Password extends Component
 {
-    /**
-     * The user's old password.
-     *
-     * @var string
-     */
     public $old_password;
 
-    /**
-     * The user's new password.
-     *
-     * @var string
-     */
     public $password;
 
-    /**
-     * The user's new password confirmation.
-     *
-     * @var string
-     */
     public $password_confirmation;
 
-    /**
-     * The validation rules.
-     *
-     * @var array
-     */
     protected $rules = [
         'password' => ['required', 'string', 'min:8', 'confirmed']
     ];
 
-    /**
-     * Render the component.
-     *
-     * @return  \Illuminate\Contracts\View\View
-     */
     public function render()
     {
         return view('profile.password');
     }
 
-    /**
-     * Update the user's password.
-     *
-     * @return  void
-     */
     public function update()
     {
         if (!$this->oldPasswordIsCorrect()) {
@@ -68,22 +38,11 @@ class Password extends Component
         $this->reset();
     }
 
-    /**
-     * Return whether the user's old password is correct.
-     *
-     * @return  bool
-     */
     protected function oldPasswordIsCorrect()
     {
         return Hash::check($this->old_password, auth()->user()->password);
     }
 
-    /**
-     * Allow for real time validation.
-     *
-     * @param   string  $attribute
-     * @return  void
-     */
     public function updated($attribute)
     {
         $this->validateOnly($attribute);

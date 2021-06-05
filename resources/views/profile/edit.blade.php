71f883e9dc7d5479a609d31555e070fbc63a76e9
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-06-05 14:02:26 +0200

    Add an escape key for the profile page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,7 @@
-<div>
+<div
+    x-data
+    @keydown.window.escape="Turbolinks.visit('{{ route('boards.index') }}');"
+>
     <div class="flex items-center justify-between mb-16">
         <h1 class="font-serif text-3xl md:text-4xl">My Profile</h1>
         <x-button class="ml-4" wire:click="logout" loading="logout">Logout</x-button>

b32241a85c88e6b4ef8a1c64017b58272e3fb385
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-06-06 11:48:15 +0200

    Update the action buttons

--- resources/views/boards/show.blade.php
+++ resources/views/boards/show.blade.php
@@ -5,20 +5,23 @@
     <x-heading>
         {{ $board->name }}
 
-        <!-- Edit button -->
-        @can('edit', $board)
-            <x-boards.action-button title="Edit this board" href="{{ route('boards.edit', $board) }}" class="-mr-4">
+        <div class="flex items-center ml-4 space-x-2">
+            <!-- Edit button -->
+            @can('edit', $board)
+                <x-boards.action-button title="Edit this board" href="{{ route('boards.edit', $board) }}">
+                    <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                    </svg>
+                </x-boards.action-button>
+            @endcan
+    
+            <!-- Members button -->
+            <x-boards.action-button title="View members" href="{{ route('members.index', $board) }}">
                 <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
                 </svg>
             </x-boards.action-button>
-        @endcan
-
-        <x-boards.action-button title="View members" href="{{ route('members.index', $board) }}">
-            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
-            </svg>
-        </x-boards.action-button>
+        </div>
     </x-heading>
 
     <!-- Show all items -->
--- resources/views/components/boards/action-button.blade.php
+++ resources/views/components/boards/action-button.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['class' => 'block ml-6 p-2 text-gray-600 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 focus:outline-none rounded-full dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500 dark:focus:bg-gray-400']) }}>
+<a {{ $attributes->merge(['class' => 'block p-2 text-gray-600 bg-gray-200 hover:bg-gray-300 focus:bg-gray-300 focus:outline-none rounded-full dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500 dark:focus:bg-gray-400']) }}>
     {{ $slot }}
 </a>

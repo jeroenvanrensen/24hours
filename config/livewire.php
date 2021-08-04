933cc79e578f49a3b3e64fbb86d8899dee5286ef
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-08-05 00:24:15 +0300

    Update Livewire config

--- config/livewire.php
+++ config/livewire.php
@@ -30,6 +30,18 @@ return [
     'view_path' => resource_path('views'),
 
     /*
+|--------------------------------------------------------------------------
+| Layout
+|--------------------------------------------------------------------------
+| The default layout view that will be used when rendering a component via
+| Route::get('/some-endpoint', SomeComponent::class);. In this case the
+| the view returned by SomeComponent will be wrapped in "layouts.app"
+|
+*/
+
+    'layout' => 'layouts.app',
+
+    /*
     |--------------------------------------------------------------------------
     | Livewire Assets URL
     |--------------------------------------------------------------------------
@@ -42,7 +54,7 @@ return [
     |
     */
 
-    'asset_url'  => null,
+    'asset_url' => null,
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +67,7 @@ return [
     |
     */
 
-    'middleware_group'  => 'web',
+    'middleware_group' => 'web',
 
     /*
     |--------------------------------------------------------------------------
@@ -77,8 +89,9 @@ return [
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs.
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
-            'jpeg', 'mpga', 'webp', 'wma',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
+        'max_upload_time' => 5, // Max duration (in minutes) before an upload gets invalidated.
     ],
 
     /*

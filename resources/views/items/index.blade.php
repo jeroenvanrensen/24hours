47986da4bbcb4f49d46d56d5f18614ac4be3769b
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-08-12 15:14:40 +0300

    Add a no items message

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -5,10 +5,6 @@
     </ul>
 
     @if(count($items) == 0)
-    <p class="-mt-16 text-center">No Items Found.</p>
-    @endif @if($showButton)
-    <div class="flex justify-center mt-6">
-        <x-button secondary class="font-semibold" wire:click="loadMore">Load More</x-button>
-    </div>
+    <p> You don't have any items yet. </p>
     @endif
 </div>

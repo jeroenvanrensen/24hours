86844dfbb7683f0658d7821c7fdc1b0a2bd6d4ea
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-08-16 11:10:06 +0300

    Fix bug

--- resources/views/components/search-result.blade.php
+++ resources/views/components/search-result.blade.php
@@ -1,10 +1,11 @@
+@if(is_numeric($loop))
 <a
     href="{{ $link }}"
     class="flex items-center w-full px-4 py-2 space-x-3  hover:bg-gray-50 focus:bg-gray-200 focus:outline-none"
     {{
     $attributes
     }}
-    @keydown.up.prevent="{{ $loop == 1 ? '$refs.input.focus()' : '$refs.search' . $loop - 1 . '.focus()' }}"
+    @keydown.up.prevent="{{ $loop == 1 ? '$refs.input.focus()' : '$refs.search' . ($loop - 1) . '.focus()' }}"
     @keydown.down.prevent="$refs.search{{ $loop + 1 }}.focus()"
     x-ref="search{{ $loop }}"
 >
@@ -14,3 +15,4 @@
 
     <span>{{ $text }}</span>
 </a>
+@endif

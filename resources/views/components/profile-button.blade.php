0a25a56ac8cfaf4201015e7f9a7d10624fd83576
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-09-05 11:09:41 +0200

    Make the profile section more responsive

--- resources/views/components/profile-button.blade.php
+++ resources/views/components/profile-button.blade.php
@@ -1,9 +1,9 @@
-<li class="mb-1">
+<li>
     <a
         href="{{ $url }}"
-        class="flex items-center px-6 py-3 space-x-3 font-medium text-gray-800 dark:text-gray-200 rounded-md {{ url()->current() === $url ? 'bg-gray-100 focus:bg-gray-200 dark:bg-gray-700 dark:focus:bg-gray-600' : 'focus:bg-gray-100 dark:focus:bg-gray-700' }}"
+        class="flex items-center px-2 -mx-2 md:px-4 py-2 md:py-3 space-x-2 md:space-x-3 font-medium text-gray-800 dark:text-gray-200 rounded-md {{ url()->current() === $url ? 'bg-gray-100 focus:bg-gray-200 dark:bg-gray-700 dark:focus:bg-gray-600' : 'focus:bg-gray-100 dark:focus:bg-gray-700' }}"
     >
-        <x-dynamic-component component="heroicon-o-{{ $icon }}" class="w-6 h-6 text-gray-700 dark:text-gray-300" />
+        <x-dynamic-component component="heroicon-o-{{ $icon }}" class="w-5 h-5 text-gray-700 md:w-6 md:h-6 dark:text-gray-300" />
         <span>{{ $text }}</span>
     </a>
 </li>
--- resources/views/components/profile-section.blade.php
+++ resources/views/components/profile-section.blade.php
@@ -1,8 +1,8 @@
 <div x-data @keydown.escape.window="!$store.modalOpen && Turbolinks.visit('{{ route('boards.index') }}')">
     <x-container>
-        <div class="md:grid md:gap-24 md:grid-cols-4">
+        <div class="md:grid md:gap-24 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
             <div>
-                <ul class="md:-mx-6">
+                <ul class="mb-8 space-y-1 md:space-y-2 md:mb-0 md:-mx-6">
                     <x-profile-button text="Profile info" icon="identification" :url="route('profile.edit')" />
                     <x-profile-button text="Avatar" icon="user-circle" :url="route('profile.avatar')" />
                     <x-profile-button text="Update password" icon="key" :url="route('profile.password')" />
@@ -10,7 +10,7 @@
                 </ul>
             </div>
 
-            <div class="col-span-3">
+            <div class="md:col-span-2 lg:col-span-3 xl:col-span-4">
                 <h3 class="mb-2 text-2xl font-semibold">{{ $title }}</h3>
                 <p class="mb-6 text-lg text-gray-700 dark:text-gray-200">{{ $description }}</p>
 

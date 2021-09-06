fc128fb4dc1c70353b4475f2d9f06547570dd457
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-09-06 11:56:59 +0200

    Make the grids more responsive

--- resources/views/boards/index.blade.php
+++ resources/views/boards/index.blade.php
@@ -5,12 +5,12 @@
             <h1 class="text-3xl font-bold ">My Boards</h1>
             <x-button @click="$dispatch('create-board')">New Board</x-button>
         </div>
-        
+
         @if(count($boards) == 0 && count($archivedBoards) == 0)
         <p x-data>You don't have any boards yet. <x-link href="javascript:;" @click="$dispatch('create-board')">Create one.</x-link></p>
         @endif
-    
-        <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+
+        <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             @foreach($boards as $board)
                 <x-card
                     :link="route('boards.show', $board)"
@@ -26,7 +26,7 @@
         @if(count($boards) > 0)
         <h1 class="mt-16 mb-10 text-3xl font-bold">Archived Boards</h1>
         @endif
-    
+
         <div class="grid grid-cols-4 gap-8">
             @foreach($archivedBoards as $board)
                 <x-card
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <ul class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+    <ul class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         @foreach($items as $item) @include('items.types.' . strtolower(class_basename($item)),
         ['board' => $board]) @endforeach
     </ul>

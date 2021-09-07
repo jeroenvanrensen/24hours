6091e2584ee5fd8d0fd3a8c2c8f47ad2dd10bb83
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2021-09-07 16:57:12 +0200

    Refactor web scraper

--- app/Services/WebScraper/Scraper.php
+++ app/Services/WebScraper/Scraper.php
@@ -2,113 +2,87 @@
 
 namespace App\Services\WebScraper;
 
+use Exception;
 use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
 class Scraper
 {
+    /**
+     * The url to be scraped.
+     */
     public string $url;
 
+    /**
+     * The web crawler to scrape the page.
+     */
     public Crawler $crawler;
 
+    /**
+     * Scrape a web page and get the title and image.
+     */
     public function scrape(string $url): object
     {
-        $this->url = $url;
-        $client = new Client();
-
         try {
-            $this->crawler = $client->request('GET', $url);
-        } catch (\Exception $e) {
+            $this->url = $url;
+            $this->crawler = (new Client())->request('GET', $this->url);
+        } catch (Exception $e) {
             return (object) ['title' => 'Not found', 'image' => null];
         }
 
         return (object) [
-            'title' => $this->getTitle(),
-            'image' => $this->getImage()
+            'title' => $this->getTitle() ?? 'Not found',
+            'image' => $this->getImage(),
         ];
     }
 
-    protected function getTitle(): string
+    /**
+     * Get the page's title from the <title> tag.
+     */
+    protected function getTitle(): ?string
     {
-        return $this->crawler->filter('title')->each(function ($node) {
-            return $node->text();
-        })[0] ?? 'Not found';
+        return $this->crawler->filter('title')->each(fn ($node) => $node->text())[0] ?? null;
     }
 
-    protected function getImage(): string|null
+    /**
+     * Get the most important image of the web page.
+     */
+    protected function getImage(): ?string
     {
-        if ($image = $this->getImageFromMetaProperty()) {
-            return $image;
-        }
-
-        if ($image = $this->getImageFromMetaName()) {
-            return $image;
-        }
-
-        return $this->getImageFromTag();
-    }
-
-    protected function getImageFromMetaProperty(): string|null
-    {
-        return $this->getAbsoluteImagePath(collect($this->crawler->filter('meta')->each(function ($node) {
-            if ($node->attr('property') == 'og:image') {
+        $imageMetas = $this->crawler->filter('meta')->each(function ($node) {
+            if ($node->attr('property') === 'og:image' || $node->attr('name') === 'og:image') {
                 return $node->attr('content');
             }
-        }))->filter()->flatten()[0] ?? null, $this->url) ?? null;
+        });
+
+        $imageTags = $this->crawler->filter('img')->each(function ($node) {
+            return $node->attr('src');
+        });
+
+        $path = collect([$imageMetas, $imageTags])->flatten()->filter()->first();
+
+        return $this->getAbsoluteImagePath($path);
     }
 
-    protected function getImageFromMetaName(): string|null
-    {
-        return $this->getAbsoluteImagePath(collect($this->crawler->filter('meta')->each(function ($node) {
-            if ($node->attr('name') == 'og:image') {
-                return $node->attr('content');
-            }
-        }))->filter()->flatten()[0] ?? null, $this->url) ?? null;
-    }
-
-    protected function getImageFromTag(): string|null
-    {
-        return $this->crawler->filter('img')->each(function ($node) {
-            return $this->getAbsoluteImagePath($node->attr('src'), $this->url);
-        })[0] ?? null;
-    }
-
-    protected function getAbsoluteImagePath($relative, $host): string|null
+    /**
+     * Get the absolute path of an image from a relative path.
+     */
+    protected function getAbsoluteImagePath(?string $relative): ?string
     {
         if (empty($relative)) {
             return null;
         }
 
-        // return if already absolute URL
-        if (parse_url($relative, PHP_URL_SCHEME) != '') {
+        if (preg_match('/https?:\/\//', $relative)) {
             return $relative;
         }
 
-        // queries and anchors
-        if (substr($relative, 0) == '#' || substr($relative, 0) == '?') {
-            return $host . $relative;
-        }
+        $path = preg_replace('#/[^/]*$#', '', parse_url($this->url)['path']); // Remove non-directory element from path
 
-        // parse host URL and convert to local variables: $scheme, $host, $path
-        extract(parse_url($host));
+        $absoluteUrl = parse_url($this->url)['host'] . $path . '/' . $relative;
 
-        // remove non-directory element from path
-        $path = preg_replace('#/[^/]*$#', '', $path);
+        $absoluteUrl = preg_replace('/\/.?\//', '/', $absoluteUrl); // Replace '//' or '/./' with '/'
 
-        // destroy path if relative url points to root
-        if (substr($relative, 0) == '/') {
-            $path = '';
-        }
-
-        // dirty absolute URL
-        $abs = $host . $path . '/' . $relative;
-
-        // replace '//' or '/./' or '/foo/../' with '/'
-        $re = array('#(/\.?/)#', '#/(?!\.\.)[^/]+/\.\./#');
-        for ($n = 1; $n > 0; $abs = preg_replace($re, '/', $abs, -1, $n)) {
-        }
-
-        // absolute URL is ready!
-        return $scheme . '://' . $abs;
+        return parse_url($this->url)['scheme'] . '://' . $absoluteUrl;
     }
 }
